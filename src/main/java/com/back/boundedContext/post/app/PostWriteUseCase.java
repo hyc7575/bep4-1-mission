024f8bdf0c6e7c14bf2ff67df3f7f8da7e97b192
kh.jeon <hyc7575@naver.com>
2026-09-17 16:28:25 +0900

    0013 commit

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,5 +1,6 @@
 package com.back.boundedContext.post.app;
 
+import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
@@ -16,10 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
+    private final MemberFacade memberFacade;
 
     @Transactional
     public RsData<Post> write (Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
+        String randomSecureTip = memberFacade.getRandomSecureTip();
 
         eventPublisher.publish(
             new PostCreatedEvent(
@@ -27,6 +30,6 @@ public class PostWriteUseCase {
             )
         );
 
-        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다. 보안 팁 : %s".formatted(post.getId(), randomSecureTip), post);
     }
 }
