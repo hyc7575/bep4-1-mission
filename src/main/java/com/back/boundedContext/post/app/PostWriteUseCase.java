5e8ec949e5499612034b835d4dd0d4d270fe9551
kh.jeon <hyc7575@naver.com>
2026-09-17 17:07:33 +0900

    0014 commit PostWriteUseCase에서 memberFacade 의존성 제거 후 api 형태로 접근

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -6,6 +6,7 @@ import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
     @Transactional
     public RsData<Post> write (Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         eventPublisher.publish(
             new PostCreatedEvent(
