b5b6e0a70557af5b08841b0d4b7dae59405cc945
kh.jeon <hyc7575@naver.com>
2026-09-17 14:54:38 +0900

    0009 commit

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -8,32 +8,23 @@ import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public long count() {
-        return postRepository.count();
-    }
-
-    public Post write(Member author, String title, String content) {
+    @Transactional
+    public Post write (Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(
-                new PostCreatedEvent(
-                        new PostDto(post)
-                )
+            new PostCreatedEvent(
+                new PostDto(post)
+            )
         );
-
         return post;
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
     }
 }
