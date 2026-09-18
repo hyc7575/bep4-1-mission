389e6f0b2b39944910875a1ee58499e06fe3d527
kh.jeon <hyc7575@naver.com>
2026-09-18 12:53:39 +0900

    0021 commit Post, PostComment 각 Entity에서 Member와의 의존성 끊기

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -26,7 +26,7 @@ public class PostFacade {
     }
 
     @Transactional
-    public RsData<Post> write(Member author, String title, String content) {
+    public RsData<Post> write(PostMember author, String title, String content) {
         return postWriteUseCase.write(author, title, content);
     }
 
@@ -48,5 +48,10 @@ public class PostFacade {
         );
 
         return postMemberRepository.save(_member);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<PostMember> findMemberByUsername(String username) {
+        return postMemberRepository.findByUsername(username);
     }
 }
--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -3,11 +3,10 @@ package com.back.boundedContext.post.app;
 import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
+import com.back.boundedContext.post.domain.PostMember;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
-import com.back.shared.member.dto.MemberDto;
-import com.back.shared.member.event.MemberModifiedEvent;
 import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
@@ -23,7 +22,7 @@ public class PostWriteUseCase {
     private final MemberApiClient memberApiClient;
 
     @Transactional
-    public RsData<Post> write (Member author, String title, String content) {
+    public RsData<Post> write (PostMember author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
         String randomSecureTip = memberApiClient.getRandomSecureTip();
 
