bf6e2743db4625f025adfb14f59ef2e3238552d7
kh.jeon <hyc7575@naver.com>
2026-09-22 17:22:05 +0900

    0044 feat: 주문 내역으로 PayoutCandidateItem 생성

--- src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
@@ -1,7 +1,24 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import com.back.boundedContext.payout.out.PayoutRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PayoutSupport {
+//	private final PayoutRepository payoutRepository;
+	private final PayoutMemberRepository payoutMemberRepository;
+
+	public Optional<PayoutMember> findSystemMember() {
+		return payoutMemberRepository.findByUsername("system");
+	}
+
+	public Optional<PayoutMember> findMemberById(int id) {
+		return payoutMemberRepository.findById(id);
+	}
 }
