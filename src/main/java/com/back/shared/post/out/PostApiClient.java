6aab8c03d6fa0b673c5b09f15510f98a710595ec
kh.jeon <hyc7575@naver.com>
2026-09-22 11:44:08 +0900

    0037 refactor: 매직넘버 및 local uri 환경변수로 분리

--- src/main/java/com/back/shared/post/out/PostApiClient.java
+++ src/main/java/com/back/shared/post/out/PostApiClient.java
@@ -1,7 +1,7 @@
 package com.back.shared.post.out;
 
-import com.back.boundedContext.post.domain.Post;
 import com.back.shared.post.dto.PostDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -10,23 +10,27 @@ import java.util.List;
 
 @Service
 public class PostApiClient {
-    private final RestClient restClient = RestClient.builder()
-            .baseUrl("http://localhost:8080/api/v1/post")
-            .build();
+  private final RestClient restClient;
 
-    public List<PostDto> getItems() {
-        return restClient.get()
-                .uri("/posts")
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {
-                });
-    }
+  public PostApiClient(@Value("${custom.global.internalBackUrl}") String internalBackUrl) {
+    this.restClient = RestClient.builder()
+        .baseUrl(internalBackUrl + "/api/v1/post")
+        .build();
+  }
 
-    public PostDto getItem(int id) {
-        return restClient.get()
-                .uri("/posts/%d".formatted(id))
-                .retrieve()
-                .body(new ParameterizedTypeReference<>() {
-                });
-    }
+  public List<PostDto> getItems() {
+    return restClient.get()
+            .uri("/posts")
+            .retrieve()
+            .body(new ParameterizedTypeReference<>() {
+            });
+  }
+
+  public PostDto getItem(int id) {
+    return restClient.get()
+            .uri("/posts/%d".formatted(id))
+            .retrieve()
+            .body(new ParameterizedTypeReference<>() {
+            });
+  }
 }
