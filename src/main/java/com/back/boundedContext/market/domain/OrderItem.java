fc6e74b27ac389f42b4367a814164ea54467c815
kh.jeon <hyc7575@naver.com>
2026-09-22 16:43:12 +0900

    0043 feat: MarketOrderPaymentCompletedEvent 이벤트 OrderItems api 통해 전달

--- src/main/java/com/back/boundedContext/market/domain/OrderItem.java
+++ src/main/java/com/back/boundedContext/market/domain/OrderItem.java
@@ -1,11 +1,14 @@
 package com.back.boundedContext.market.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.market.dto.OrderItemDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.FetchType.LAZY;
 
@@ -34,5 +37,24 @@ public class OrderItem extends BaseIdAndTime {
         this.productName = productName;
         this.price = price;
         this.salePrice = salePrice;
+    }
+
+    public OrderItemDto toDto() {
+        return new OrderItemDto(
+            getId(),
+            getCreateDate(),
+            getModifyDate(),
+            order.getId(),
+            order.getBuyer().getId(),
+            order.getBuyer().getNickname(),
+            product.getSeller().getId(),
+            product.getSeller().getNickname(),
+            product.getId(),
+            productName,
+            price,
+            salePrice,
+            payoutRate
+        );
+
     }
 }
