e9e69b3527f86306d8f8d2d92e8e5939ae987c10
kh.jeon <hyc7575@naver.com>
2026-09-22 15:40:59 +0900

    0041 feat: PayoutMember 생성 및 정산후보 등록 이벤트 발행

---
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
@@ -0,0 +1,7 @@
+package com.back.boundedContext.payout.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayoutSupport {
+}
---
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSyncMemberUseCase.java
@@ -0,0 +1,44 @@
+package com.back.boundedContext.payout.app;
+
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.member.dto.MemberDto;
+import com.back.shared.payout.event.PayoutMemberCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PayoutSyncMemberUseCase {
+	private final PayoutMemberRepository payoutMemberRepository;
+	private final EventPublisher eventPublisher;
+
+	public PayoutMember syncMember(MemberDto member) {
+		boolean isNew = !payoutMemberRepository.existsById(member.getId());
+
+		PayoutMember _member = payoutMemberRepository.save(
+				new PayoutMember(
+						member.getId(),
+						member.getCreateDate(),
+						member.getModifyDate(),
+						member.getUsername(),
+						"",
+						member.getNickname(),
+						member.getActivityScore()
+				)
+		);
+
+		if (isNew) {
+			eventPublisher.publish(
+					new PayoutMemberCreatedEvent(
+							_member.toDto()
+					)
+			);
+		}
+
+		return _member;
+	}
+}
